deabaa3c9d73c1b010de97ad154e3cc8f884aa78
SpielerEinzig <SpielerEinzig@gmail.com>
2023-01-18 09:51:01 +0100

    integrated database with UI

--- lib/providers/user_provider.dart
+++ lib/providers/user_provider.dart
@@ -1,42 +1,17 @@
-import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
-import 'package:firebase_cloud_functions/models/user_model.dart';
 import 'package:firebase_cloud_functions/services/auth_service.dart';
 import 'package:firebase_cloud_functions/services/database_service.dart';
 import 'package:flutter/material.dart';
 
-final FirebaseFirestore _firestore = FirebaseFirestore.instance;
 final AuthService _authService = AuthService();
 final DatabaseService _databaseService = DatabaseService();
-
-UserModel _userModel = UserModel(
-  running: false,
-  amount: 0,
-  email: "email",
-  userid: "",
-);
 
 User? _userAuthCredential;
 
 class UserProvider with ChangeNotifier {
-  UserModel get getUserModel => _userModel;
+  User? get getUserCredential => _userAuthCredential;
 
-  _listenUserDetails() {
-    _firestore
-        .collection("Users")
-        .doc(_userModel.userid)
-        .snapshots()
-        .listen((event) {
-      final json = event.data()!;
-
-      _userModel.running = json['running'];
-      _userModel.amount = json['amount'];
-      _userModel.email = json['email'];
-      _userModel.userid = json['userid'];
-    });
-  }
-
-  registerUser({
+  Future registerUser({
     required String email,
     required String password,
   }) async {
@@ -51,11 +26,13 @@ class UserProvider with ChangeNotifier {
         email: email,
       );
 
-      _listenUserDetails();
+      return true;
+    } else {
+      return false;
     }
   }
 
-  loginUser({
+  Future<bool> loginUser({
     required String email,
     required String password,
   }) async {
@@ -65,12 +42,9 @@ class UserProvider with ChangeNotifier {
     );
 
     if (_userAuthCredential != null) {
-      await _databaseService.registerUser(
-        userid: _userAuthCredential!.uid,
-        email: email,
-      );
-
-      _listenUserDetails();
+      return true;
+    } else {
+      return false;
     }
   }
 }
--- lib/screens/sign_up_screen.dart
+++ lib/screens/sign_up_screen.dart
@@ -1,4 +1,6 @@
+import 'package:firebase_cloud_functions/providers/user_provider.dart';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 
 import '../utilities/page_navigation.dart';
 import '../widgets/custom_button.dart';
@@ -16,6 +18,7 @@ class _SignUpScreenState extends State<SignUpScreen> {
   final TextEditingController _emailController = TextEditingController();
   final TextEditingController _passwordController = TextEditingController();
   final PageNavigation _navigation = PageNavigation();
+  bool loading = false;
   @override
   Widget build(BuildContext context) {
     final size = MediaQuery.of(context).size;
@@ -32,11 +35,13 @@ class _SignUpScreenState extends State<SignUpScreen> {
               child: Column(
                 children: [
                   CustomTextField(
+                    obscureText: false,
                     controller: _emailController,
                     label: "Enter your email",
                   ),
                   const SizedBox(height: 30),
                   CustomTextField(
+                    obscureText: true,
                     controller: _passwordController,
                     label: "Enter your password",
                   ),
@@ -44,11 +49,27 @@ class _SignUpScreenState extends State<SignUpScreen> {
               ),
             ),
             CustomButton(
-              onPressed: () {
-                _navigation.pushPage(
-                  context: context,
-                  destination: const HomeScreen(),
-                );
+              loading: loading,
+              onPressed: () async {
+                setState(() {
+                  loading = true;
+                });
+
+                bool proceed = await context.read<UserProvider>().registerUser(
+                      email: _emailController.text,
+                      password: _passwordController.text,
+                    );
+
+                if (proceed) {
+                  setState(() {
+                    loading = false;
+                  });
+
+                  _navigation.pushPage(
+                    context: context,
+                    destination: const HomeScreen(),
+                  );
+                }
               },
               label: "Sign up",
               height: size.height * 0.075,
